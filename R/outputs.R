d00d78312ec76f6d01671474dc57fc766514ad49
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2024-12-23 11:51:57 +1100

    Update output helper docs

--- R/outputs.R
+++ R/outputs.R
@@ -1,3 +1,19 @@
+outputs_factory <- function(class, field, vec = FALSE) {
+  force(class)
+  force(field)
+  force(vec)
+  function() {
+    outputs <- get(".evaluate_result", parent.frame(2))
+    res <- Filter(\(x) inherits(x, class), outputs)
+    res <- lapply(res, `[[`, field)
+    if(vec) {
+      unlist(res, recursive = FALSE)
+    } else {
+      res
+    }
+  }
+}
+
 #' Extract Common Output Types from an Evaluate Object
 #'
 #' These functions facilitate the extraction of specific output types, such as results, errors, warnings,
@@ -12,10 +28,9 @@
 #' - **Warnings**: Extracts the `message` field from objects of class `"warning"`.
 #' - **Messages**: Extracts the `message` field from objects of class `"message"`.
 #'
-#' The `outputs_factory()` function serves as a helper to create these specialised extractors.
-#'
 #' @param class Character. The class of objects to extract from the `evaluate` output.
 #' @param field Character. The field within the object to extract (e.g., `"value"` or `"message"`).
+#' @param vec Logical. Whether the results should be unlisted into a vector or not.
 #'
 #' @return A list containing the extracted fields from the specified class of outputs.
 #'
@@ -41,24 +56,6 @@
 #'
 #' @name outputs-extraction
 #' @aliases outputs results errors warnings messages
-#' @export
-outputs_factory <- function(class, field, vec = FALSE) {
-  force(class)
-  force(field)
-  force(vec)
-  function() {
-    outputs <- get(".evaluate_result", parent.frame(2))
-    res <- Filter(\(x) inherits(x, class), outputs)
-    res <- lapply(res, `[[`, field)
-    if(vec) {
-      unlist(res, recursive = FALSE)
-    } else {
-      res
-    }
-  }
-}
-
-#' @rdname outputs-extraction
 #' @export
 ql_outputs <- function(class, field) outputs_factory(class, field)()
 
